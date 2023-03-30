920b9a341e6e9fd9f674c588a1528d0e6c77a031
Jerinjoseph07 <122715467+Jerinjoseph07@users.noreply.github.com>
2023-03-30 03:11:44 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Sampleform from './components/Sampleform';
+import Singleform from './components/Singleform';
+import Validation from './components/Validation';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Validation></Validation>
+      
+      
+    </div>
+  );
+}
+
+export default App;
